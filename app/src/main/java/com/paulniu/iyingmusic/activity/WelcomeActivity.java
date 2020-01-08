9ce02dab98f65fd035d55383e1d9bc3ddbe49d43
paulniu <niupuyue@aliyun.com>
2020-01-09 00:00:34 +0800

    code format

--- app/src/main/java/com/paulniu/iyingmusic/activity/WelcomeActivity.java
+++ app/src/main/java/com/paulniu/iyingmusic/activity/WelcomeActivity.java
@@ -1,11 +1,27 @@
 package com.paulniu.iyingmusic.activity;
 
+import com.paulniu.iyingmusic.base.BaseActivity;
+
 /**
  * Coder: niupuyue (牛谱乐)
  * Date: 2020-01-05
  * Time: 23:09
- * Desc:
+ * Desc: 首次安装的欢迎页面
  * Version:
  */
-public class WelcomeActivity {
+public class WelcomeActivity extends BaseActivity {
+    @Override
+    public int initViewLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initViewById() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
 }
